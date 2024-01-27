18ff2a50788d3bd9b67dc0c141821443277b706d
溶度积Ksp <rdjksp@gmail.com>
2024-01-27 20:55:17 +0800

    init

--- App.tsx
+++ App.tsx
@@ -5,9 +5,7 @@ import {Camera} from "expo-camera";
 export default function App() {
     return (
         <View>
-            <StatusBar style="auto"/>
-            <Text>Open up App.tsx to start working on your app!</Text>
-            <Camera useCamera2Api={true} barCodeScannerSettings={{barCodeTypes: ["qr"]}}
+            <Camera barCodeScannerSettings={{barCodeTypes: ["qr"]}}
                     style={{height: "80%", width: "100%", marginTop: "10%"}}/>
             <Button title={"GET PERMISSIONS"} onPress={() => {
                 Camera.requestCameraPermissionsAsync().then();
