76af4d77deb4667f027778e9bde02e4d3ee48281
溶度积Ksp <rdjksp@gmail.com>
2024-01-27 20:49:01 +0800

    init

--- App.tsx
+++ App.tsx
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {Button, Text, View} from 'react-native';
+import {Camera} from "expo-camera";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View>
+            <StatusBar style="auto"/>
+            <Text>Open up App.tsx to start working on your app!</Text>
+            <Camera useCamera2Api={true} barCodeScannerSettings={{barCodeTypes: ["qr"]}}
+                    style={{height: "80%", width: "100%", marginTop: "10%"}}/>
+            <Button title={"GET PERMIS"} onPress={() => {
+                Camera.requestCameraPermissionsAsync().then();
+            }}/>
+        </View>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
